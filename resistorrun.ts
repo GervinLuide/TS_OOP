813f0f9786b6be37fcc38f3ff1e79771b3698a3d
Gervin Luide <gervin.luide@khk.ee>
2023-01-13 16:55:43 +0200

    TS-5 -  jadaühenduse arvutamine klassiga

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,41 +1,69 @@
 class Resistor {
     r: number = 0;
-    m: number = 0;
-    constructor(r: number, m: number) {
+    constructor(r: number) {
         this.r = r;
-        this.m = m;
     }
     getCurrent(u: number): number {
         return u / this.r;
     }
-    getPower(): number {
-        return this.m * this.getCurrent(this.m);
+    getPower(u: number): number {
+        return u * this.getCurrent(u);
     }
-    getMax():number{
-        return Math.sqrt(this.getPower() * this.r)
-    }
-    checkMax():boolean{
-        return this.m <= this.getMax()
+    getResistance(): number {
+        return this.r;
     }
 }
 
-let r1 = new Resistor(440, 10);
-let r2 = new Resistor(440, 10);
-let r3 = new Resistor(440, 10);
-let r4 = new Resistor(440, 10);
-let r5 = new Resistor(440, 10);
-let resistors: Resistor[] = [r1,r2,r3,r4,r5]
-function filterResistors(): Resistor[]{
-    let allow: Resistor[] = [];
-    resistors.forEach((resistorr) => {
-        if (resistorr.checkMax()){
-            allow.push(resistorr)
-        }
-    })
-    return allow
+class SeriesCircuit {
+    resistors: Resistor[] = []
+    push(r: Resistor) {
+        this.resistors.push(r);
+    }
+    getTotalResistance() {
+        let sum: number = 0;
+        this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
+        return sum;
+    }
+    getCurrent(u: number) {
+        return u / this.getTotalResistance();
+    }
+    getTotalPower(u: number) {
+        let current = this.getCurrent(u);
+        let power = 0;
+        this.resistors.forEach((r: Resistor) => power += r.getPower(current));
+        return power;
+    }
+    getHighestResistance(): number {
+        return Math.max(...this.resistors.map(resistor => resistor.getResistance()));
+    }
+    getHighestVoltageDrop(totalVoltage: number): number {
+        let current = this.getCurrent(totalVoltage);
+        return Math.max(...this.resistors.map(resistor => current * resistor.getResistance()));
+    }
+    getHighestPowerDissipation(totalVoltage: number): number {
+        let current = this.getCurrent(totalVoltage);
+        return Math.max(...this.resistors.map(resistor => current * current * resistor.getResistance()));
+    }
+
 }
 
-console.log(r1.getPower());
-console.log(r1.checkMax())
-console.log(r1.getMax())
-console.log(filterResistors())
+let sc1: SeriesCircuit = new SeriesCircuit();
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(110));
+
+
+//let sc2: SeriesCircuit = new SeriesCircuit();
+//sc2.push(new Resistor(310))
+//sc2.push(new Resistor(310))
+//sc2.push(new Resistor(210))
+
+console.log(sc1.getTotalResistance());
+console.log(sc1.getCurrent(12));
+console.log(sc1.getTotalPower(12));
+console.log(sc1.getHighestResistance());
+console.log(sc1.getHighestVoltageDrop(5));
+console.log(sc1.getHighestPowerDissipation(5));
+
+//console.log(sc2.getTotalResistance());
+//console.log(sc2.getCurrent(19));
