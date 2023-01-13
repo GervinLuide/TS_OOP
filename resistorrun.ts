4846548ffd8b84ef1530c57e38f5a0fca76df745
Gervin Luide <gervin.luide@khk.ee>
2023-01-13 09:25:26 +0200

    TS-3 - Võimsuse arvutamine objektiga

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,46 +1,41 @@
 class Resistor {
     r: number = 0;
-    constructor(r: number) {
+    m: number = 0;
+    constructor(r: number, m: number) {
         this.r = r;
+        this.m = m;
     }
     getCurrent(u: number): number {
         return u / this.r;
     }
-    getPower(u: number): number {
-        return u * this.getCurrent(u);
+    getPower(): number {
+        return this.m * this.getCurrent(this.m);
     }
-    getAmps(w:number, v:number): number{
-        return w/v
+    getMax():number{
+        return Math.sqrt(this.getPower() * this.r)
     }
-    getOhms(v:number, w:number): number{
-        return v / this.getAmps(w, v)
+    checkMax():boolean{
+        return this.m <= this.getMax()
     }
-    getWater(w:number, ml:number): number{
-        return Math.floor(20 + (60/(4.19*(ml / w))))
-    }
-
-
 }
-//mitu vatti soojust eraldub takistist?
-let r1 = new Resistor(2.5);
-console.log(r1.getPower(5));
 
-//mitu amprit voolu läbib takistit?
-let r2 = new Resistor(2.5);
-console.log(r2.getAmps(6, 4));
+let r1 = new Resistor(440, 10);
+let r2 = new Resistor(440, 10);
+let r3 = new Resistor(440, 10);
+let r4 = new Resistor(440, 10);
+let r5 = new Resistor(440, 10);
+let resistors: Resistor[] = [r1,r2,r3,r4,r5]
+function filterResistors(): Resistor[]{
+    let allow: Resistor[] = [];
+    resistors.forEach((resistorr) => {
+        if (resistorr.checkMax()){
+            allow.push(resistorr)
+        }
+    })
+    return allow
+}
 
-//Mitu oomi on eelneva takisti takistus?
-console.log(r2.getOhms(4, 6))
-
-//Mitme kraadi peale tõuseb vee temperatuur 20 kraadi
-//Celsiuse pealt ühe minutiga, kui kogu sisselülitatud
-// kannu võimsus läheb vee soojendamiseks?
-
-console.log(r2.getWater(1000, 1000))
-
-//Mitu amprit voolu läbib eelnimetatud veekeedukannu, kui võrgupinge on 220 volti?
-console.log(r1.getAmps(1000, 220))
-
-//Mitu oomi on selle veekeedukannu takistus?
-
-console.log(r1.getOhms(220, 1000))
+console.log(r1.getPower());
+console.log(r1.checkMax())
+console.log(r1.getMax())
+console.log(filterResistors())
