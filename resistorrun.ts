58dedf667817b8dc984cdfae865a641731e63429
Gervin Luide <gervin.luide@khk.ee>
2023-01-17 13:21:48 +0200

    Ülesanne TS-4 - jadaühenduse arvutamine

--- resistorrun.ts
+++ resistorrun.ts
@@ -24,25 +24,19 @@ class SeriesCircuit {
         this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
         return sum;
     }
+    getPower(u:number){
+       return u * this.getCurrent(u)
+    }
+
     getCurrent(u: number) {
         return u / this.getTotalResistance();
     }
-    getTotalPower(u: number) {
-        let current = this.getCurrent(u);
-        let power = 0;
-        this.resistors.forEach((r: Resistor) => power += r.getPower(current));
-        return power;
-    }
-    getHighestResistance(): number {
-        return Math.max(...this.resistors.map(resistor => resistor.getResistance()));
-    }
-    getHighestVoltageDrop(totalVoltage: number): number {
-        let current = this.getCurrent(totalVoltage);
-        return Math.max(...this.resistors.map(resistor => current * resistor.getResistance()));
-    }
-    getHighestPowerDissipation(totalVoltage: number): number {
-        let current = this.getCurrent(totalVoltage);
-        return Math.max(...this.resistors.map(resistor => current * current * resistor.getResistance()));
+    getallPower(u: number){
+        let power: any = []
+        this.resistors.forEach((r: Resistor) => {
+            power.push(r.getPower(u))
+        })
+        return power
     }
 
 }
@@ -50,20 +44,9 @@ class SeriesCircuit {
 let sc1: SeriesCircuit = new SeriesCircuit();
 sc1.push(new Resistor(220));
 sc1.push(new Resistor(220));
-sc1.push(new Resistor(110));
-
-
-//let sc2: SeriesCircuit = new SeriesCircuit();
-//sc2.push(new Resistor(310))
-//sc2.push(new Resistor(310))
-//sc2.push(new Resistor(210))
-
+sc1.push(new Resistor(220));
 console.log(sc1.getTotalResistance());
 console.log(sc1.getCurrent(12));
-console.log(sc1.getTotalPower(12));
-console.log(sc1.getHighestResistance());
-console.log(sc1.getHighestVoltageDrop(5));
-console.log(sc1.getHighestPowerDissipation(5));
+console.log(sc1.getPower(12));
+console.log(sc1.getallPower(12))
 
-//console.log(sc2.getTotalResistance());
-//console.log(sc2.getCurrent(19));
