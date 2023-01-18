5eb38b998d8057a930f2618e674280b8d080dc6e
Gervin Luide <gervin.luide@khk.ee>
2023-01-18 13:43:08 +0200

    Ülesanne TS-7 - alamklass

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,17 +1,92 @@
-let resistors:number[]=[110, 220, 220];
-let inversesum:number=0;
-for(let resistance of resistors){
-    inversesum+=1/resistance;
+class Resistor {
+    r: number = 0;
+    constructor(r: number) {
+        this.r = r;
+    }
+    getResistance(): number {
+        return this.r;
+    }
 }
-let amps = 0.5
-let totalResistance = 1 / inversesum;
-let voltage = 4.5;
-let current = 1;
-let power = voltage * current;
-let ohms = voltage / amps;
-console.log("Power: " + power + " watts, Resistance: " + totalResistance + " ohms");
-let secondtotalResistance = 0.5 / inversesum;
-let secondcurrent = 0.5;
-let secondPower = voltage * secondcurrent;
-console.log("Secondary Power(tagatuli): " + secondPower + " watts, Secondary Resistance: " + secondtotalResistance + " ohms");
-console.log(ohms)
+
+class Switch extends Resistor {
+    constructor() {
+        super(0);
+    }
+    on() {
+        this.r = 0;
+    }
+    off() {
+        this.r = 1000000000;
+    }
+}
+
+class SeriesCircuit {
+    resistors: Resistor[] = []
+    push(r: Resistor) {
+        this.resistors.push(r);
+    }
+    getTotalResistance() {
+        let sum: number = 0;
+        this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
+        return sum;
+    }
+    getCurrent(u: number) {
+        return u / this.getTotalResistance();
+    }
+    getPower(u: number): number {
+        return u * this.getCurrent(u);
+    }
+}
+class parallelcircuit{
+    resistors: Resistor[] = []
+    push(r: Resistor) {
+        this.resistors.push(r);
+    }
+    getTotalResistance(){
+        let sum: number = 0;
+        this.resistors.forEach((r: Resistor) => {
+            if(r.r > 0){
+                sum += 1 / r.r
+            }
+        });
+        return sum;
+    }
+}
+
+//let r1: Resistor = new Resistor(220);
+//let sw1: Switch = new Switch();
+//let sw2: Switch = new Switch();
+//sw1.off();
+//sw2.off();
+
+//let sc1: SeriesCircuit = new SeriesCircuit();
+//sc1.push(r1);
+//sc1.push(sw1);
+//sc1.push(sw2);
+//console.log(sc1.getTotalResistance());
+//console.log(sc1.getCurrent(5));
+//console.log(sc1.getPower(5))
+//sw1.on();
+//console.log(sc1.getTotalResistance());
+//console.log(sc1.getCurrent(5));
+//console.log(sc1.getPower(5))
+//sw2.on();
+//console.log(sc1.getTotalResistance());
+//console.log(sc1.getCurrent(5));
+//console.log(sc1.getPower(5))
+
+let r1: Resistor = new Resistor(220);
+let sw1: Switch = new Switch();
+let sw2: Switch = new Switch();
+let pc1:parallelcircuit = new parallelcircuit();
+pc1.push(r1)
+pc1.push(sw1)
+pc1.push(sw2)
+sw1.off()
+sw2.off()
+console.log(pc1.getTotalResistance())
+sw1.on()
+console.log(pc1.getTotalResistance())
+sw2.on()
+console.log(pc1.getTotalResistance())
+
